6b6d7a7207bae77abc6a333458f0322455062342
Joel Koch <joel.koch5@gmail.com>
2017-10-20 17:44:08 +0300

    Sidemenu enchantments

--- content/themes/custom/models/shared/sidenav.php
+++ content/themes/custom/models/shared/sidenav.php
@@ -18,17 +18,28 @@ class Sidenav extends \DustPress\Model {
             'order'             => 'ASC',
         ];
         foreach ( $menu as $key => $item ) {
+            $curpage = get_the_ID();
+            $curpageparent = wp_get_post_parent_id();
             $args['post_parent'] = $item->object_id;
             $children = \DustPress\Query::get_posts( $args );
             if ( is_array($children) && count( $children ) > 0 ) {
                 $menu[ $key ]->has_children = true;
                 $menu[ $key ]->children = $children;
+                if ( $curpageparent === (int) $menu[ $key ]->object_id ) {
+                    $menu[ $key ]->current_first = "opened";
+                    $menu[ $key ]->current_id = $curpage;
+                }
                 foreach ( $menu[ $key ]->children as $child_key => $children ) {
                     $args['post_parent'] = $children->ID;
                     $sub_children = \DustPress\Query::get_posts( $args );
                     if ( count( $sub_children ) > 0 ) {
                         $menu[ $key ]->children[ $child_key ]->has_sub_children = true;
                         $menu[ $key ]->children[ $child_key ]->children = $sub_children;
+                        if ( $curpageparent ===  $menu[ $key ]->children[ $child_key ]->ID ) {
+                            $menu[ $key ]->current_first = "opened";
+                            $menu[ $key ]->children[ $child_key ]->current_second = "opened";
+                            $menu[ $key ]->children[ $child_key ]->current_id = $curpage;
+                        }
                     }
                 }
             }
