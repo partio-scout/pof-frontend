137e8759b55c1553c0232e25865755d1d70a4b84
Kalle Haavisto <kalle.haavisto@geniem.com>
2018-04-10 08:07:56 +0300

    search routing

---
+++ content/themes/custom/functions/function-search.php
@@ -0,0 +1,37 @@
+<?php
+add_action( 'init', 'page_rewrite', 999, 0 );
+/**
+ * Rewrite the loadmore
+ */
+function page_rewrite() {
+    // This rule matches /aktiviteettihaku/{search clause}(/sivu/{page number}/)
+    add_rewrite_rule( '^aktiviteettihaku/([^/]+)(/sivu/([^/]+))?(/)?$', 'index.php?s=$matches[1]&paged=$matches[2]', 'top' );
+}
+add_action( 'init', 'translate_base_rewrite', 999, 0 );
+
+/**
+ * This changes the default pagination and search base.
+ */
+function translate_base_rewrite() {
+    global $wp_rewrite;
+    $wp_rewrite->pagination_base = 'sivu';
+    $wp_rewrite->search_base     = 'aktiviteettihaku';
+}
+add_action( 'template_redirect', 'search_redirect' );
+
+/**
+ * Redirects search results from /?s=query to /search/query/, converts %20 to +
+ *
+ * @link http://txfx.net/wordpress-plugins/nice-search/
+ */
+function search_redirect() {
+    global $wp_rewrite;
+    if ( ! isset( $wp_rewrite ) || ! is_object( $wp_rewrite ) || ! $wp_rewrite->get_search_permastruct() ) {
+        return;
+    }
+    $search_parameter = filter_input( INPUT_GET, 's', FILTER_SANITIZE_URL );
+    if ( is_search() && ! is_admin() && ! empty( $search_parameter ) ) {
+        wp_safe_redirect( get_search_link() );
+        exit();
+    }
+}
