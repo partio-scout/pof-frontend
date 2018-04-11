a68a3a321644ffe2586318389f32b26a3e7ea4a7
Kalle Haavisto <kalle.haavisto@geniem.com>
2018-04-11 12:50:47 +0300

    change aktiviteettihaku path to haku

--- content/themes/custom/functions/function-search.php
+++ content/themes/custom/functions/function-search.php
@@ -5,8 +5,8 @@ add_action( 'init', 'page_rewrite', 999, 0 );
  */
 function page_rewrite() {
     global $wp_rewrite;
-    // This rule matches /aktiviteettihaku/{search clause}(/sivu/{page number}/)
-    add_rewrite_rule( '^aktiviteettihaku/([^/]+)(/sivu/([^/]+))?(/)?$', 'index.php?s=$matches[1]&paged=$matches[2]', 'top' );
+    // This rule matches /haku/{search clause}(/sivu/{page number}/)
+    add_rewrite_rule( '^haku/([^/]+)(/sivu/([^/]+))?(/)?$', 'index.php?s=$matches[1]&paged=$matches[2]', 'top' );
     
     // This rule matches /guid/{post guid}/)
     add_rewrite_rule( 'guid/(.+)', 'index.php?pagename=guid&guid=$matches[1]', 'top' );
@@ -18,7 +18,7 @@ add_action( 'init', 'translate_base_rewrite', 999, 0 );
 function translate_base_rewrite() {
     global $wp_rewrite;
     $wp_rewrite->pagination_base = 'sivu';
-    $wp_rewrite->search_base     = 'aktiviteettihaku';
+    $wp_rewrite->search_base     = 'haku';
 }
 add_action( 'template_redirect', 'search_redirect' );
 /**
