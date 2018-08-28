e2815d66df9b516cc82b36d465d03d55d632f640
arttu <arttu.makiporhola@geniem.com>
2018-08-28 16:43:05 +0300

    tip share functionality

--- content/themes/custom/models/shared/tips.php
+++ content/themes/custom/models/shared/tips.php
@@ -12,25 +12,32 @@ class Tips extends \DustPress\Model {
             return;
         }
         $tips_args = [
-            'post_id'   => get_the_ID(),    // id of the curr page
-            'status' => 'approve',
-            'orderby'   => 'comment_date',
-            'order'     => 'ASC'
+            'post_id' => get_the_ID(),    // id of the curr page
+            'status'  => 'approve',
+            'orderby' => 'comment_date',
+            'order'   => 'ASC',
         ];
-        $tips = get_comments( $tips_args );
-        foreach ($tips as $key => $tip) {
-            $i++;
-            $tips[$key]->comment_content = nl2br($tips[$key]->comment_content);
-            $tips[$key]->fields = get_comment_meta($tip->comment_ID);
+        $tips      = get_comments( $tips_args );
+        foreach ( $tips as &$tip ) {
+            $tip->comment_content = nl2br( $tip->comment_content );
+            $tip->fields          = get_comment_meta( $tip->comment_ID );
 
-            if (isset($tips[$key]->fields['attachments'])) {
-                $j++;
-                $tips[$key]->fields['attachments'] = json_decode_pof($tips[$key]->fields['attachments'][0]);
+            // Parse tip guid from ag_{guid}_{lang}
+            foreach ( $tip->fields as $key => $value ) {
+                $key_arr = explode( '_', $key );
+                if ( count( $key_arr ) === 3 && $key_arr[0] === 'ag' ) {
+                    $tip->guid = $key_arr[1];
+                    break;
+                }
+            }
 
-                foreach ($tips[$key]->fields['attachments'] as $type => $attachment) {
-                    if ($type == 'files') {
-                        foreach ($attachment as $attachment_key => $file) {
-                            $tips[$key]->fields['attachments']->{$type}[$attachment_key]->icon = get_template_directory_uri() . '/assets/img/file_'.substr($file->url, -3).'.png';
+            if ( isset( $tip->fields['attachments'] ) ) {
+                $tip->fields['attachments'] = json_decode_pof( $tip->fields['attachments'][0] );
+
+                foreach ( $tip->fields['attachments'] as $type => $attachment ) {
+                    if ( $type === 'files' ) {
+                        foreach ( $attachment as $attachment_key => $file ) {
+                            $tip->fields['attachments']->{$type}[ $attachment_key ]->icon = get_template_directory_uri() . '/assets/img/file_' . substr( $file->url, -3 ) . '.png';
                         }
                     }
                 }
@@ -40,7 +47,7 @@ class Tips extends \DustPress\Model {
     }
 
     public function SendUrl() {
-        return get_field('tips-send-url', 'option');
+        return get_field( 'tips-send-url', 'option' );
     }
 
     // Init strings for UI
@@ -52,18 +59,18 @@ class Tips extends \DustPress\Model {
         }
 
         $s = [
-            'vinkit'            => __( 'Tips', 'pof' ),
-            'jarjestys'         => __( 'Order', 'pof' ),
-            'uusimmat'          => __( 'Latest', 'pof' ),
-            'suosituimmat'      => __( 'Most popular', 'pof' ),
-            'nayta_lisaa'       => __( 'Show more', 'pof' ),
-            'lisaa_vinkki'      => __( 'Add a tip', 'pof' ),
-            'kuvat'             => __( 'Pictures', 'pof' ),
-            'laheta'            => __( 'Send', 'pof' ),
-            'nimi'              => __( 'Name', 'pof'),
-            'otsikko'           => __( 'Title', 'pof'),
-            'kirjoita_tahan'    => __( 'Write your tips here', 'pof'),
-            'liite'             => __( 'Add an attachment', 'pof')
+            'vinkit'         => __( 'Tips', 'pof' ),
+            'jarjestys'      => __( 'Order', 'pof' ),
+            'uusimmat'       => __( 'Latest', 'pof' ),
+            'suosituimmat'   => __( 'Most popular', 'pof' ),
+            'nayta_lisaa'    => __( 'Show more', 'pof' ),
+            'lisaa_vinkki'   => __( 'Add a tip', 'pof' ),
+            'kuvat'          => __( 'Pictures', 'pof' ),
+            'laheta'         => __( 'Send', 'pof' ),
+            'nimi'           => __( 'Name', 'pof' ),
+            'otsikko'        => __( 'Title', 'pof' ),
+            'kirjoita_tahan' => __( 'Write your tips here', 'pof' ),
+            'liite'          => __( 'Add an attachment', 'pof' ),
         ];
 
         return $s;
