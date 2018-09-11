3566b63604f8bdef9e3ccf939f805b382b31eee1
arttu <arttu.makiporhola@geniem.com>
2018-09-11 11:52:06 +0300

    optimize menu data retrieval & remove ini_set

--- content/themes/custom/functions/class-menu.php
+++ content/themes/custom/functions/class-menu.php
@@ -43,29 +43,32 @@ class Menu {
 
         // only add items to the main menu and its translations
         if (
-            substr( $menu->slug, 0, strlen( static::$slug ) ) === static::$slug &&
-            ! is_admin()
+            ! is_admin() &&
+            substr( $menu->slug, 0, strlen( static::$slug ) ) === static::$slug
         ) {
             $cache_key = 'custom_menu/' . $menu->slug;
             $new_items = wp_cache_get( $cache_key );
             if ( empty( $new_items ) ) {
-                ini_set( 'memory_limit', '256M' );
 
                 // Add the children
-                $new_items = static::add_children( $items );
+                $items = static::add_children( $items );
 
-                wp_cache_set( $cache_key, $new_items, null, HOUR_IN_SECONDS );
+                wp_cache_set( $cache_key, $items, null, HOUR_IN_SECONDS );
             }
-
-            // Replace item list with new items
-            if ( ! empty( $new_items ) ) {
+            else {
                 $items = $new_items;
             }
 
             // Mark current active tree
-            $parent_menu_id = $items[ $items[ get_the_ID() ]->menu_item_parent ]->ID ?? null;
-            if ( $parent_menu_id ) {
-                $items = static::mark_active( $items, $parent_menu_id );
+            $id = get_the_ID();
+            if (
+                array_key_exists( $id, $items ) &&
+                property_exists( $items[ $id ], 'menu_item_parent' ) &&
+                array_key_exists( $items[ $id ]->menu_item_parent, $items ) &&
+                property_exists( $items[ $items[ $id ]->menu_item_parent ], 'ID' )
+            ) {
+                $parent_menu_id = $items[ $items[ $id ]->menu_item_parent ]->ID;
+                $items          = static::mark_active( $items, $parent_menu_id );
             }
         }
 
@@ -81,43 +84,41 @@ class Menu {
     private static function add_children( $items ) {
 
         // Modify array so the id is the array key
-        $menu_items = [];
-        foreach ( $items as &$post ) {
-            $menu_items[ $post->ID ] = $post;
-        }
+        $items = array_combine( wp_list_pluck( $items, 'ID' ), $items );
 
         // Add menu item children automatically
-        foreach ( $menu_items as &$post ) {
+        foreach ( $items as &$post ) {
             if ( $post->object === 'page' ) {
                 $query = ( new \WP_Query([
                     'post_parent'    => $post->object_id,
-                    'post_type'      => $post->object,
+                    'post_type'      => 'page',
                     'post_status'    => 'publish',
                     'posts_per_page' => -1, //phpcs:ignore
+                    'fields'         => 'ids',
                 ]) );
                 if ( ! empty( $query->posts ) ) {
                     foreach ( $query->posts as &$child_post ) {
                         $child_post = static::custom_nav_menu_item( $child_post, $post->ID );
 
                         // Add the child posts immediately so they get checked for children as well
-                        $menu_items[ $child_post->ID ] = $child_post;
+                        $items[ $child_post->ID ] = $child_post;
                     }
                 }
             }
         }
 
         // Add submenu classes
-        foreach ( $menu_items as &$post ) {
+        foreach ( $items as &$post ) {
             $id = $post->menu_item_parent ?: null;
             if (
-                array_key_exists( $id, $menu_items ) &&
-                ! in_array( 'menu-item-has-children', $menu_items[ $id ]->classes, true )
+                array_key_exists( $id, $items ) &&
+                ! in_array( 'menu-item-has-children', $items[ $id ]->classes, true )
             ) {
-                $menu_items[ $id ]->classes[] = 'menu-item-has-children';
+                $items[ $id ]->classes[] = 'menu-item-has-children';
             }
         }
 
-        return $menu_items;
+        return $items;
     }
 
     /**
@@ -139,36 +140,25 @@ class Menu {
     }
 
     /**
-     * Helper function to change posts into menu items
+     * Helper function to change post id's into menu items
      *
-     * @param  \WP_Post $post   Post item.
-     * @param  int      $parent Post item parent menu id.
-     * @return \WP_Post         Modified $post.
+     * @param  int $post_id Post id.
+     * @param  int $parent  Post item parent menu id.
+     * @return stdClass     Modified $post.
      */
-    private static function custom_nav_menu_item( \WP_Post $post, int $parent = 0 ) {
-        $post->object_id        = $post->ID;
-        $post->object           = $post->post_type;
-        $post->type             = 'post_type';
-        $post->post_type        = 'nav_menu_item';
-        $post->menu_item_parent = $parent;
-        $post                   = wp_setup_nav_menu_item( $post );
-
-        // Only keep necessary values
-        $whitelist = [
-            'ID',
-            'menu_order',
-            'menu_item_parent',
-            'url',
-            'title',
-            'classes',
-            'object_id',
-            'object',
+    private static function custom_nav_menu_item( int $post_id, int $parent = 0 ) {
+        $post = (object) [
+            'ID'               => $post_id,
+            'object_id'        => $post_id,
+            'object'           => 'page',
+            'type'             => 'post_type',
+            'post_type'        => 'nav_menu_item',
+            'menu_item_parent' => $parent,
+            'menu_order'       => get_post_field( 'menu_order', $post_id ),
+            'url'              => get_permalink( $post_id ),
+            'title'            => get_the_title( $post_id ),
+            'classes'          => [],
         ];
-        foreach ( (array) $post as $key => $val ) {
-            if ( ! in_array( $key, $whitelist, true ) ) {
-                unset( $post->{ $key } );
-            }
-        }
 
         return $post;
     }
