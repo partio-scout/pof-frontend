76f8a0a6859df9be6a7b9e882763205f019a6283
arttu <arttu.makiporhola@geniem.com>
2018-12-17 13:17:03 +0200

    fix language switcher

--- content/themes/custom/models/shared/program-langnav.php
+++ content/themes/custom/models/shared/program-langnav.php
@@ -2,81 +2,67 @@
 
 class ProgramLangnav extends \DustPress\Model {
 
-    public $current_lang;
+    /**
+     * Currently selected lang slug
+     *
+     * @var string
+     */
+    public static $current_lang = '';
 
-    public function Lang() {
-        $post = \DustPress\Query::get_acf_post( get_the_ID() );
+    /**
+     * Get language switcher data
+     *
+     * @return array Language list.
+     */
+    public function Lang() : array {
+        $langs = pll_the_languages([
+            'echo'                   => false,
+            'raw'                    => true,
+            'hide_if_no_translation' => true,
+        ]);
 
-        // If the post is not created by the api, we have to get the translations otherway.
-       if (empty($post->fields['api_guid'])) {
-            $langs = pll_the_languages([
-                'echo' => 0,
-                'raw' => 1,
-                'hide_if_no_translation' => 1,
-            ]);
-            foreach ($langs as $key => $value) {
-                $lang['langs'][$key]['lang']        = strtoupper($value['slug']);
-                $lang['langs'][$key]['permalink']   = $value['url'];
-                if($value['current_lang']) $lang['langs'][$key]['class']   = 'active ';
-
-            }
-            return $lang;
-        }
-
-        $args = [
-            'posts_per_page'    => -1,
-            'post_type'         => 'page',
-            'post_status'       => 'publish',
-            'meta_key'          => 'api_guid',
-            'meta_value'        => $post->fields['api_guid'],
-            'lang'              => '',
+        $lang = [
+            'langs' => array_map( [ __CLASS__, 'map_languages' ], $langs ),
         ];
-        $pages  = \DustPress\Query::get_acf_posts( $args );
-        $lang = [];
 
-        // Get main languages
-        $languages = function_exists('pll_languages_list') ? pll_languages_list() : array();
-
-        // Get the parent model name.
-        $model_args     = $this->get_args();
-        $parent_model   = $model_args['model'];
-        // Iterator for non-top-level-languages
-        $exist_extra_langs = 0;
-
-        foreach ($pages as $page) {
-            // Force api created pages to right lang order, ksort below before return languages
-            $key = array_search( strtolower( $page->fields['api_lang'] ), $languages );
-
-            // If page is not top level language, generate key after bae languages
-            if ($key === false) {
-                $key = count($languages) + $exist_extra_langs;
-                $exist_extra_langs++;
-            }
-            $lang['langs'][$key]['id']          = $page->ID;
-            $lang['langs'][$key]['lang']        = $page->fields['api_lang'];
-            $lang['langs'][$key]['permalink']   = $page->permalink;
-            $lang['langs'][$key]['model']       = $parent_model;
-
-            // Build class string and store current language
-            if ( $post->fields['api_lang'] == $page->fields['api_lang'] ) {
-                $lang['langs'][$key]['class']   = 'active ';
-                $this->current_lang         = strtolower( $post->fields['api_lang'] );
-                add_filter( 'body_class', [ $this, 'add_lang_to_body_class' ] );
-            }
-
-            // This page does not belong to a top level language
-            if ( ! in_array( strtolower( $page->fields['api_lang'] ), $languages, true ) ) {
-                $lang['langs'][$key]['class'] .= 'dynamic';
-            }
-
-            trim( $lang['langs'][$key]['class'] );
-        }
-        ksort($lang['langs']);
         return $lang;
     }
 
-    public function add_lang_to_body_class( $classes ) {
-            $classes['lang'] = $this->current_lang;
-            return $classes;
+    /**
+     * Map default language list
+     *
+     * @param  array $lang Language to modify.
+     * @return array       Modified $lang.
+     */
+    public static function map_languages( array $lang ) : array {
+        $lang['lang'] = strtoupper( $lang['slug'] );
+
+        if ( $lang['current_lang'] ) {
+            $lang['class']        = 'active ';
+            static::$current_lang = $lang['slug'];
+            add_filter( 'body_class', [ __CLASS__, 'add_lang_to_body_class' ] );
+        }
+
+        if ( is_search() ) {
+            $current = rawurlencode( search_base( pll_current_language() ) );
+            $default = rawurlencode( search_base( 'fi' ) );
+
+            $lang['url'] = str_replace( $current, rawurlencode( search_base( $lang['slug'] ) ), $lang['url'] );
+        }
+
+        $lang['permalink'] = $lang['url'];
+
+        return $lang;
+    }
+
+    /**
+     * Add current language to body classes
+     *
+     * @param  array $classes Current classes.
+     * @return array          Modified $classes.
+     */
+    public static function add_lang_to_body_class( array $classes ) : array {
+        $classes['lang'] = static::$current_lang;
+        return $classes;
     }
 }
