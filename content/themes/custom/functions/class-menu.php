e4f4ed8cd894e2df1758f83eb610176e0f016997
arttu <arttu.makiporhola@geniem.com>
2019-01-11 11:37:03 +0200

    enable all languages in menu language switcher

--- content/themes/custom/functions/class-menu.php
+++ content/themes/custom/functions/class-menu.php
@@ -22,7 +22,32 @@ class Menu {
      */
     public static function init() {
         add_action( 'init', [ __CLASS__, 'register_nav_menu' ] );
-        add_filter( 'wp_get_nav_menu_items', [ __CLASS__, 'wp_get_nav_menu_items' ], 2, 20 );
+        add_filter( 'wp_get_nav_menu_items', [ __CLASS__, 'wp_get_nav_menu_items' ], 20, 2 );
+        add_filter( 'wp_get_nav_menu_items', [ __CLASS__, 'switcher_title' ], 200 );
+    }
+
+    /**
+     * Modify language switcher titles
+     *
+     * @param  array $items Menu items.
+     * @return array        Modified $items.
+     */
+    public static function switcher_title( array $items ) : array {
+        $items = array_map( [ __CLASS__, 'map_switcher_title' ], $items );
+        return $items;
+    }
+
+    /**
+     * Modify language switcher titles
+     *
+     * @param  \WP_Post|\stdClass $item Menu item to change.
+     * @return \WP_Post|\stdClass       Modified $item.
+     */
+    public static function map_switcher_title( $item ) {
+        if ( property_exists( $item, 'lang' ) ) {
+            $item->title = explode( '-', $item->lang )[0] ?? $item->title;
+        }
+        return $item;
     }
 
     /**
@@ -39,7 +64,7 @@ class Menu {
      * @param  \WP_Term $menu  Menu that is being modified.
      * @return array           Modified $items.
      */
-    public static function wp_get_nav_menu_items( array $items, \WP_Term $menu ) {
+    public static function wp_get_nav_menu_items( array $items, \WP_Term $menu ) : array {
 
         // only add items to the main menu and its translations
         if (
@@ -82,7 +107,7 @@ class Menu {
      * @param  array $items Items to seek children for.
      * @return array        Modified $items.
      */
-    private static function add_children( $items ) {
+    private static function add_children( array $items ) : array {
 
         // Modify array so the id is the array key
         $items = array_combine( wp_list_pluck( $items, 'ID' ), $items );
@@ -125,10 +150,11 @@ class Menu {
     /**
      * Mark current tree items as active
      *
-     * @param array $items Items to search.
-     * @param int   $id    Id to search for.
+     * @param  array $items Items to search.
+     * @param  int   $id    Id to search for.
+     * @return array        Modified $items.
      */
-    private static function mark_active( array $items, int $id ) {
+    private static function mark_active( array $items, int $id ) : array {
         if ( array_key_exists( $id, $items ) ) {
             $item            = $items[ $id ];
             $item->classes[] = 'opened';
@@ -145,9 +171,9 @@ class Menu {
      *
      * @param  int $post_id Post id.
      * @param  int $parent  Post item parent menu id.
-     * @return stdClass     Modified $post.
+     * @return \stdClass    Modified $post.
      */
-    private static function custom_nav_menu_item( int $post_id, int $parent = 0 ) {
+    private static function custom_nav_menu_item( int $post_id, int $parent = 0 ) : \stdClass {
         $post = (object) [
             'ID'               => $post_id,
             'object_id'        => $post_id,
